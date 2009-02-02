0a273ccf0db439c6fe55a126e46a7395dd6e16dd
stephen.larson <stephen.larson@933566eb-c141-0410-b91b-f3a7fcfc7766>
2009-02-02 10:48:22 +0000

    new version of categoryfixer

--- trunk/slarson/PyWikipediaOWLTools/src/CategoryFixerBot.py
+++ trunk/slarson/PyWikipediaOWLTools/src/CategoryFixerBot.py
@@ -18,9 +18,9 @@ def findCapsVersion(allCapsVersion):
 
     words = allCapsVersion.split()
     if len(words) > 5:
-        words[0] = words[0].upper()
-        words[1] = words[1].upper()
-        words[2] = words[2].upper()
+        words[0] = words[0].capitalize()
+        words[1] = words[1].capitalize()
+        words[2] = words[2].capitalize()
         words[3] = words[3].lower()
         words[4] = words[4].lower()
         words[5] = words[5].lower()
